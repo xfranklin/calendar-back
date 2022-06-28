fc5b1d5d5dc96c00a1c019a8d9ae2f3c64161540
dfeleniuk <d.feleniuk@oooi.app>
2022-06-28 23:56:43 +0300

    [development] (feat): config ci/cd

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(3000);
