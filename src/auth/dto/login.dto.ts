c3d589d0bfbb5d25fc10a96107c0537815f5a81f
dfeleniuk <d.feleniuk@oooi.app>
2022-08-13 13:11:07 +0300

    [development] (fix): fix auth dto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -11,9 +11,9 @@ export class LoginDto {
   @IsNotEmpty({ message: "EMAIL_EMPTY" })
   email: string;
 
-  @MinLength(6, { message: "SHORT_PASSWORD" })
-  @MaxLength(64, { message: "LONG_PASSWORD" })
-  @Matches(/^[A-Za-z0-9~`!@#$%^&*()_\-+={[}\]|\:;"'<,>.?\/]*$/, {
+  @MinLength(4, { message: "SHORT_PASSWORD" })
+  @MaxLength(128, { message: "LONG_PASSWORD" })
+  @Matches(/^[A-Za-z0-9~`!@#$%^&*()_\-+={[}\]|:;"'<,>.?\/]*$/, {
     message: "PASSWORD_UNSUSPECTED_CHARACTERS",
   })
   @IsNotEmpty({ message: "PASSWORD_EMPTY" })
